720c86aa52e94bf9ca257dfcbf347ce44875456d
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2025-06-30 02:26:14 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Manana/',
   plugins: [react(),
     tailwindcss(),
   ],
-   server: {
-    host: '192.168.222.108',  // Allow external devices to connect
-    port: 5173,       // Or any port you prefer
-  },
+   
 })
