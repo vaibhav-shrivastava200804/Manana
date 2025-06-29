a11441c36618b7b04e3e991da3f42478c1bb3ba9
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2025-06-30 02:54:29 +0530

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import MainContent from './components/mainContent';
 import ForgiveMe from './components/forgiveMe/ForgiveMe';
 import OurMoments from './components/ourMoments/OurMoments';
@@ -6,11 +6,13 @@ import './App.css';
 
 function App() {
   return (
+    <Router>
     <Routes>
       <Route path="/Manana/" element={<MainContent />} />
       <Route path="Manana/Forgive_Me" element={<ForgiveMe />} />
       <Route path="Manana/Our_Moments" element={<OurMoments />} />
     </Routes>
+    </Router>
   );
 }
 
